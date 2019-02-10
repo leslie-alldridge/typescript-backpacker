943a2fef913efe2afbc733521e770d947a3d59b7
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-11 06:38:41 +1300

    removed empty col

--- migrations/20190206084659_bags.js
+++ migrations/20190206084659_bags.js
@@ -2,7 +2,6 @@ exports.up = knex =>
   knex.schema.createTable("bags", table => {
     table.increments("id").primary();
     table.string("username");
-    table.string("bag");
     table.string("destination");
     table.string("description");
   });
