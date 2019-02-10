0059c71f8233280e12e3a15735776f96d0d00b75
leslie-alldridge <leslie.alldridge@gmail.com>
2019-02-11 04:30:03 +1300

    added pg start script

--- webpack.config.js
+++ webpack.config.js
@@ -55,17 +55,10 @@ module.exports = {
       }
     ]
   },
-  // For development https://webpack.js.org/configuration/devtool/#for-development
-  devtool: "inline-source-map",
-  devServer: {
-    port: 8080,
-    noInfo: true
-  },
   plugins: [
-    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: "index.html", //Name of file in ./dist/
-      template: "index.html", //Name of template in ./src
+      filename: "index.html",
+      template: "index.html",
       hash: true
     }),
     new MiniCssExtractPlugin({
